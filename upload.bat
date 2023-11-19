fe8fc46999285bca61e1f9885bc0e16092821b17
lancerstadium <lancerstadium@163.com>
2023-11-19 17:57:24 +0800

    2023/11/19 周日 17:57:24.47

--- upload.bat
+++ upload.bat
@@ -1,10 +1,5 @@
 @echo off
 set commit_message=%*
-if "%commit_message%"=="" (
-  set /p commit_message="请输入提交信息: "
-)
 git add .
 git commit -m "%date% %time% %commit_message%"
 git push
-echo 提交完成！
-pause
